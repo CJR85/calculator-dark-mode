53ee1917d12fd64d115b47ae20bf4d9e66c6f61c
Calvin Roberts <roberts.calvin85@gmail.com>
2022-09-22 21:46:00 +0100

    Create function for buttons

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+let buttons = document.querySelector('.buttons'),
+  btn = document.querySelectorAll('span'),
+  value = document.getElementById('value');
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click', function () {
+    if (this.innerHTML == '=') {
+      value.innerHTML = eval(value.innerHTML);
+    } else {
+      if (this.innerHTML == 'Clear') {
+        value.innerHTML = '';
+      } else {
+        value.innerHTML += this.innerHTML;
+      }
+    }
+  });
+}
